bd8763d9b56e96061309bcf6a1cc670cf86380fc
reilly <tang.ruilin@foxmail.com>
2023-06-22 17:53:34 +0800

    路由变量名修改
    Signed-off-by: reilly <tang.ruilin@foxmail.com>

--- src/proxy/proxy_api.rs
+++ src/proxy/proxy_api.rs
@@ -32,74 +32,74 @@ async fn overview() -> HttpResponse {
 }
 
 #[get("/model")]
-async fn model(data: web::Data<ApiServer>) -> impl Responder {
+async fn model(_data: web::Data<ApiServer>) -> impl Responder {
     HttpResponse::Ok().body("model")
 }
 
 #[get("/stats")]
-async fn stats_no_xauth(data: web::Data<ApiServer>) -> impl Responder {
+async fn stats_no_xauth(_data: web::Data<ApiServer>) -> impl Responder {
     HttpResponse::Ok().body("stats_no_xauth")
 }
 
 #[get("/slots")]
-async fn slots_no_xauth(data: web::Data<ApiServer>) -> impl Responder {
+async fn slots_no_xauth(_data: web::Data<ApiServer>) -> impl Responder {
     HttpResponse::Ok().body("slots_no_xauth")
 }
 
 #[get("/xping/{xauth}")]
-async fn x_ping(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn x_ping(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("x_ping")
 }
 
 // todo: 路由或许要做一些适配
 // #[get("stats/{xauth}")]
 #[get("/stats/{xauth}/{flags}")]
-async fn stats(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn stats(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("stats")
 }
 
 #[get("/slots/{xauth}")]
-async fn slots(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn slots(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("slots")
 }
 
 #[put("/start/{xauth}")]
-async fn start(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn start(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("start")
 }
 
 #[put("/stats/reset/{xauth}")]
-async fn reset_stats(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn reset_stats(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("reset_stats")
 }
 
 #[put("/forcegc/{xauth}")]
-async fn force_gc(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn force_gc(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("force_gc")
 }
 
 #[put("/shutdown/{xauth}")]
-async fn shutdown(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn shutdown(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("shut_down")
 }
 
 #[put("/loglevel/{xauth}/{value}")]
-async fn log_level(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn log_level(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("log_level")
 }
 
 #[put("/fillslots/{xauth}")]
-async fn fill_slots(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn fill_slots(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("fill_slots")
 }
 
 #[put("/sentinels/{xauth}")]
-async fn set_sentinels(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn set_sentinels(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("set_sentinels")
 }
 
 #[put("/sentinels/{xauth}")]
-async fn rewatch_sentinels(data: web::Data<ApiServer>, info: web::Path<Info>) -> impl Responder {
+async fn rewatch_sentinels(_data: web::Data<ApiServer>, _info: web::Path<Info>) -> impl Responder {
     HttpResponse::Ok().body("rewatch_sentinels")
 }
 
